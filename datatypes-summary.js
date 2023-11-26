d0db37cb1b7cae16edd343fd560e64fce9c58835
Dead lord <150676849+tahabinyasir@users.noreply.github.com>
2023-11-26 08:11:21 +0000

    Datatypes-Summary

---
+++ datatypes-summary.js
@@ -0,0 +1,35 @@
+//  Primitive
+
+// 7 Types : String, Number, Boolean, Null, Undifined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const IsLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherid = Symbol('123')
+
+console.log(id == anotherid);
+
+// const bigNumber = 546546256646566345346n
+
+
+
+// Reference (Non-primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"]
+let myobj = {
+    name: "Taha",
+    age: 14,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof bigNumber);
