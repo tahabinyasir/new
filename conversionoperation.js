e01f41fe0ac0a84b2b0aef1716ba8661caecb1d9
Dead lord <150676849+tahabinyasir@users.noreply.github.com>
2023-11-26 07:34:26 +0000

    conversionoperation

--- conversionoperation.js
+++ conversionoperation.js
@@ -25,5 +25,35 @@ let booleanIsLoggedIn = Boolean (IsLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ****************** Operations ***********************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Hello"
+let str2 = " Taha"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log(1 + 2 + "2");
+
+console.log(true);
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter)
