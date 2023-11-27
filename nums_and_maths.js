367c499db952962dc28203145f9b31527df6366e
Dead lord <150676849+tahabinyasir@users.noreply.github.com>
2023-11-27 15:22:18 +0000

    nums_and_maths

---
+++ nums_and_maths.js
@@ -0,0 +1,28 @@
+const score = 400
+
+const balance = new Number(100)
+// console.log(balance);
+// console.log(score);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 23.8966
+
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-ID'));
+
+// ++++++++++++++++++++++++++++++++  Maths  +++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-1000));
+// console.log(Math.round(4.5))
+// console.log(Math.ceil(4.6))
+// console.log(Math.floor(4.9))
+
+// console.log(Math.min(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+// console.log(Math.max(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+
+console.log(Math.random());
