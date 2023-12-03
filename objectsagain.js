722b4b23b86846f72a3012de5480d66207cce7e8
Dead lord <150676849+tahabinyasir@users.noreply.github.com>
2023-12-03 06:00:20 +0000

    objectsagain

---
+++ objectsagain.js
@@ -0,0 +1,54 @@
+// const facebookUser = new Object()
+const facebookUser = {}
+
+facebookUser.id = "123abc"
+facebookUser.name = "Deadlord"
+facebookUser.isLooggedIn = false
+facebookUser.email = "taha@gmail.com"
+facebookUser.fullname = "Taha Bin Yasir"
+
+// console.log(facebookUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+                firstname: "Taha",
+                lastname: "Yasir"
+        }
+    } 
+}
+
+// console.log(regularUser);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+// const obj3 = {5: "a", 6: "b"}
+
+// const obj3 = { obj2, obj1 }
+
+// const obj4 = Object.assign({}, obj1, obj2, obj3)
+
+const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+
+
+const users = [
+    {
+        id: 1,
+        email: "h@gmail"
+    },
+    {
+        id: 1,
+        email: "h@gmail"
+    },
+]
+
+users[1].email
+console.log(facebookUser);
+
+console.log(Object.keys(facebookUser));
+console.log(Object.values(facebookUser));
+console.log(Object.entries(facebookUser));
+
+console.log(Object.hasOwnProperty(isLooggedIn));
