d790b97a0ae6ec4f625689a55bf907a34ac1e7d1
Dead lord <150676849+tahabinyasir@users.noreply.github.com>
2023-12-09 14:22:16 +0000

    one,truthy,iterations

--- one.js
+++ one.js
@@ -1,5 +1,51 @@
 // if
+// <, >, <=, >=, ==, !=, ===, !==
 
-if (true) {
-    
+
+// const isUserLoggedIn = true
+// const temperature = 49
+
+
+// if ( temperature < 50 ) {
+//     console.log("Less than 50%");
+// } else {
+// console.log("Temperature is greater than 50%");
+// }
+
+
+// const score = 200
+
+// if (score > 100) {
+//     const power = "fly"
+//     console.log(`User Power ${power}`);
+// }
+
+
+const balance = 1000
+
+// if (balance > 500) console.log("Taha"), console.log("Taha2");
+// Imature code
+
+// if (balance < 500) {
+//     console.log("Less than 500");
+// } else if (balance < 750) {
+//     console.log("Less than 750");
+// } else if (balance < 900) {
+//    console.log("Less than 900"); 
+// } else if (balance < 1200) {
+//    console.log("Less than 1200"); 
+// }
+
+
+const userLoggedIN = true
+const debitCard = true
+const loggedInfromGoogle = false
+const loggedInfromEmail = true
+
+if (userLoggedIN && debitCard) {
+    console.log("Allow to buy courses");
+}
+
+if (loggedInfromGoogle || loggedInfromEmail) {
+    console.log("User logged in");
 }
--- truthy.js
+++ truthy.js
@@ -1,16 +1,17 @@
-const userEmail = ""
-
-if (userEmail) {
-    console.log("Got user email");
-} else {
-    console.log("Dont have user email");
-}
-
 // Falsy values
 // false, 0, -0, BigInt 0n, "", null, undefined, NaN
 
 // Truthy values
 // true, "0", 'false', " ", [], {}, function(){}
+
+
+const userEmail = "gth"
+
+if (userEmail) {
+    console.log("Got user email");
+} else {
+    console.log("Don't have user email"); 
+}
 
 if (userEmail.length === 0) {
     console.log("Array is empty");
@@ -21,3 +22,18 @@ const emptyObj = {}
 if (Object.keys(emptyObj).length === 0) {
     console.log("Object is empty");
 }
+
+
+// let val1;
+// val1 = 5 ?? 10
+// console.log(val1);
+
+
+// Terniary Operator
+
+
+// condition ? true : false
+
+
+const iceTeaPrice = 200
+iceTeaPrice <= 180 ? console.log("Less than 180") : console.log("Greater than 180");
